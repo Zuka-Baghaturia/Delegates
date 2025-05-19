2af9bed2992c59673908314ec9125bd489b177a0
User <User@DESKTOP-8KK9T6M>
2025-05-19 10:45:32 -0700

    Add project files.

---
+++ Delegates/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+
+List<string> words = new List<string> { "Luka", "Natali", "Zurabi", "Levana", "Dato", "Lasha" };
+
+Predicate<string> predicate = (x) => x.Length > 5;
+
+List<string> filtered = words.FindAll(predicate);
+
+Action<string> printUpper = (x) => Console.WriteLine(x.ToUpper());
+
+filtered.ForEach(printUpper);
+
+
+
+MyDelegate myDelegate = PrintSum;
+
+myDelegate(3, 4);
+
+static void PrintSum(int a, int b)
+{
+    Console.WriteLine(a + b);
+}
+
+public delegate void MyDelegate(int x, int y);
